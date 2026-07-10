64d351b6bfb8e0285abf62576349c1f8acb46461
AlphaXIV012 <167891996+AlphaXIV012@users.noreply.github.com>
2026-07-10 15:30:58 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+const name = "Praize"
+const age = 22
+const favouriteCodingLanguage = "Python"
+
+function showDetails(){
+    console.log("Hello! My name is " + name + ", I am " + age + " years old and I love " + favouriteCodingLanguage);
+}
+
+showDetails()
