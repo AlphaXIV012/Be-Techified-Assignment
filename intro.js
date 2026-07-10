a32eaefc83adacd81433beb043a1129e2855b9db
AlphaXIV012 <167891996+AlphaXIV012@users.noreply.github.com>
2026-07-10 16:19:47 +0100

    assignment

--- intro.js
+++ intro.js
@@ -1,9 +1,14 @@
-const name = "Praize"
-const age = 22
-const favouriteCodingLanguage = "Python"
+// const name = "Praize"
+// const age = 22
+// const favouriteCodingLanguage = "Python"
 
-function showDetails(){
-    console.log("Hello! My name is " + name + ", I am " + age + " years old and I love " + favouriteCodingLanguage);
-}
+// function showIntro(){
+//     console.log("Hello! My name is " + name + ", I am " + age + " years old and I love " + favouriteCodingLanguage);
+// }
 
-showDetails()
+// showIntro()
+
+
+const intro = (name, age, favouriteCodingLanguage) => "Hello! My name is " + name + ", I am " + age + " years old and I love " + favouriteCodingLanguage
+
+console.log(intro("Praize", 22, "Python"))
